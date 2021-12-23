a1b293d4f95de68c27b343fe6c819d32f0667971
Lydon-01 <lydoncarter@gmail.com>
2021-12-23 13:52:49 +0200

    Update s3ObjectSearchAndCount.py
    Corrected method used.

--- s3ObjectSearchAndCount.py
+++ s3ObjectSearchAndCount.py
@@ -1,16 +1,14 @@
 # The function should take two arguments:
-bucket_name =  "mybucket" # the name of the S3 bucket to search
-prefix= "p_day"  # a prefix string to filter the S3 objects
+bucket_name = "mybucket" # the name of the S3 bucket to search
+prefix= "myString"  # a string to find in the S3 objects
 
-# The function should return a list of all S3 objects that match the search criteria.
-
+# The function will count all S3 objects that match the search criteria.
 import boto3
 s3 = boto3.resource('s3')
 
-def count_s3_objects(bucket_name, prefix):
-    bucket = s3.Bucket(bucket_name)
-    objects = [o for o in bucket.objects.filter(Prefix=prefix)]
-    return objects
-
-print(count_s3_objects(bucket_name, prefix))
-
+bucket = s3.Bucket(bucket_name)
+count = 0
+for obj in bucket.objects.all():
+   if prefix in obj.key:
+       count += 1
+print(count)
